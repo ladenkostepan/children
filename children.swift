f69c0a01bcb9caeec93ea6a5684f2b56a62a58bc
Deborah <152811078+ladenkostepan@users.noreply.github.com>
2024-02-04 17:54:40 +0200

    Create children.swift

---
+++ children.swift
@@ -0,0 +1,6 @@
+      root.children.addAll(moveLeftButton, moveRightButton, playerRectangle)
+        val scene = Scene(root, 800.0, 600.0)
+        primaryStage.title = "Kotlin Scratch Game"
+        primaryStage.scene = scene
+        primaryStage.show()
+    }
